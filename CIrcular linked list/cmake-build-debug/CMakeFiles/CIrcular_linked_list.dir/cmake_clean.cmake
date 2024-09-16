4778d7a31d790b4777f842299d1a7ce787284e3a
Kashan Ali Khalid <kashanali_khalid@yahoo.com>
2024-09-16 06:28:35 -0700

    Add files via upload

---
+++ CIrcular linked list/cmake-build-debug/CMakeFiles/CIrcular_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CIrcular_linked_list.dir/main.cpp.obj"
+  "CIrcular_linked_list.pdb"
+  "CIrcular_linked_list.exe"
+  "CIrcular_linked_list.exe.manifest"
+  "libCIrcular_linked_list.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CIrcular_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
